8fe0b14159298e747e225a8e43bea12fe916897b
Rekha Tenjarla <rekha.tenjarla@gmail.com>
2016-03-06 20:53:03 -0800

    test2

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 var twitter = require('twit');
 
-test
-
 module.exports = new twitter({
      consumer_key: 'qfxuGZKQF8DtdZAB8oHHltsDQ',
      consumer_secret: 'lEbRO3q0L5YoMUz8fwLQ0yfvHfjKgLD3smTSLdVC90TUYFpokr',
      access_token: '23701093-7MGYRnLFjUOMGlkEKIy2KzrYNRi2baJ1e02vH7PGu',
      access_token_secret: 'CebOsppcGbXowQF5lqbQ78K9Dm8R8bpsCMyfwGRcfEJ1G'
    });
+
+// asdfasfdasf
